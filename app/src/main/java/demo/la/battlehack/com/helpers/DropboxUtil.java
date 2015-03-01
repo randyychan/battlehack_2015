65c3101f5e75ba4765d695438929a73b1799c148
Sutardji <kevin_sutardji@intuit.com>
2015-03-01 02:36:16 -0800

    db libs

---
+++ app/src/main/java/demo/la/battlehack/com/helpers/DropboxUtil.java
@@ -0,0 +1,35 @@
+package demo.la.battlehack.com.helpers;
+
+import android.content.Context;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AppKeyPair;
+
+/**
+ * Created by ksutardji on 3/1/15.
+ */
+public enum DropboxUtil {
+    INSTANCE;
+
+    private static final String DROPBOX_APP_KEY = "7e6llv53ut0cewx";
+    private static final String DROPBOX_APP_SECRET = "v43rd7t45jmi6dq";
+
+    private static DropboxAPI<AndroidAuthSession> mDBApi;
+
+    static {
+        // And later in some initialization function:
+        AppKeyPair appKeys = new AppKeyPair(DROPBOX_APP_KEY, DROPBOX_APP_SECRET);
+        AndroidAuthSession session = new AndroidAuthSession(appKeys);
+        mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+
+    }
+
+    public void auth(Context context) {
+        mDBApi.getSession().startOAuth2Authentication(context);
+    }
+
+    public AndroidAuthSession getDropboxSession() {
+        return mDBApi.getSession();
+    }
+}
